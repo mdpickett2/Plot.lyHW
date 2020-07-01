e593ef856ccff7aa4facebfbce959ac3d3c3bb37
mdpickett <mdpickett2@gmail.com>
2020-07-01 18:40:03 -0500

    update2

--- static/js/app.js
+++ static/js/app.js
@@ -13,12 +13,25 @@ function buildBarchart() {
     var tooltip = data.samples.map(row =>row["otu_labels"]);
     var sampleValues = data.samples.map(row => row["sample_values"]);
     var labels = data.samples.map(row => row["otu_ids"]);
-  
+    
+    //convert labels to string, split between objects
+    var strlabels = labels[0].toString().split(",");
+    //map otu id's back onto newLabels array
+    var newLabels = strlabels.map(element => `OTU ${element}`);
+
+    
+
+    // console.log(id);
+    // console.log(tooltip);
+    // console.log(sampleValues);
+    // console.log(labels);
+
     
     //creating trace for bar plot and slicing data to keep only top 10 values
     var trace1 = {
+      
       x: sampleValues[0].slice(0, 10).reverse(),
-      y: labels.slice(0, 10).reverse(),
+      y: newLabels.slice(0, 10).reverse(),
       text: tooltip[0].slice(0,10).reverse(),
       type: "bar",
       orientation: "h",
@@ -27,6 +40,7 @@ function buildBarchart() {
 
     // creating array of trace 1 for adding to newPlot function
     var top10data = [trace1];
+    console.log(top10data);
 
     //layout for bar chart
     var layout = {
